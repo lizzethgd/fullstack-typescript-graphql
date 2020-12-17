cc165d3fb94b373c135f529ba27bc87157a5f589
lizzethgd <lizzethgd@gmail.com>
2020-12-17 13:32:40 +0200

    configured server 1

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,15 @@
 import { Connection, EntityManager, IDatabaseDriver } from "@mikro-orm/core";
+import {Request, Response} from "express"
+import { Session } from 'express-session';
+
+declare module 'express-session' {
+    interface SessionData {
+      userId: number;
+    }
+  }
 
 export type MyContext = {
     em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
+    req: Request & { session: Session}
+    res: Response
 }
